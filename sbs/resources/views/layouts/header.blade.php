684800a41ab2a9ca46f6e7d7342ecd6315b9ff1f
ucim13 <slr.knm@gmail.com>
2023-08-12 17:59:32 +0700

    sbs_lara

---
+++ sbs/resources/views/layouts/header.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>@yield('title')</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  
+  <link href="{{asset('style')}}/assets/img/favicon.png" rel="icon">
+  <link href="{{asset('style')}}/assets/img/apple-touch-icon.png" rel="icon">
+ 
+  <!-- Google Fonts -->
+  <link href="https://fonts.gstatic.com" rel="preconnect">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+
+  <link href="{{asset('style')}}/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="{{asset('style')}}/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="{{asset('style')}}/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="{{asset('style')}}/assets/vendor/quill/quill.snow.css" rel="stylesheet">
+  <link href="{{asset('style')}}/assets/vendor/quill/quill.bubble.css" rel="stylesheet">
+  <link href="{{asset('style')}}/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="{{asset('style')}}/assets/vendor/simple-datatables/style.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="{{asset('style')}}/assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: NiceAdmin
+  * Updated: May 30 2023 with Bootstrap v5.3.0
+  * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
